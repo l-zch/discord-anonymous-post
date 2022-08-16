a916019481736e880aedae38b9ffdeeec7bfaeef
__tsengchen__ <89698082+tseng-Chen@users.noreply.github.com>
2022-08-16 23:32:20 +0800

    add Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,24 @@
 <template>
-    <a href="https://discord.com/api/oauth2/authorize?client_id=1008762136706682921&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code&scope=identify%20guilds">Login to discord</a>
+  <!-- <a href="https://discord.com/api/oauth2/authorize?client_id=1008762136706682921&redirect_uri=https%3A%2F%2Ftseng-chen.github.io%2Fdiscord-anonymous-post%2F&response_type=code&scope=identify%20guilds">
+        連接DISCORD
+    </a> -->
+
+  <div
+    class="m-auto flex h-auto w-[80vw] max-w-[480px] flex-col items-center bg-[#36393f] p-[32px] rounded-[5px] shadow-md"
+  >
+    <img
+      src="https://play-lh.googleusercontent.com/0oO5sAneb9lJP6l8c6DH4aj6f85qNpplQVHmPmbbBxAukDnlO7DarDW0b-kEIHa8SQ"
+      class="h-[64px] w-[64px] rounded-[16px] mb-[27px]"
+    />
+    <text class="text-[#b9bbbe]">__tseng__ 邀請您加入</text>
+    <b class="mt-[10px] text-[50px] text-white leading-[50px] mb-[40px]">匿名貓咪</b>
+    <a
+      href="https://discord.com/api/oauth2/authorize?client_id=1008762136706682921&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdiscord-anonymous-post%2F&response_type=code&scope=identify%20guilds"
+      class="flex h-[50px] w-full rounded-[3px] bg-[#41B883] text-[20px] text-white"
+    >
+      <text class="m-auto text-[18px]"> 接受邀請 </text>
+    </a>
+  </div>
 </template>
 
-<script>
-
-</script>
+<script></script>
